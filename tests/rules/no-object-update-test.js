8abbb824012ad7a82ff49a19f8ca52abdaec874d
Santosh Sutar <ssutar@ssutar-mn4.linkedin.biz>
2018-10-11 23:51:05 -0700

    Make the rule configurable to the usage of utility methods
    For details: https://github.com/ssutar/eslint-plugin-ember-object-update/issues/1

--- tests/rules/no-object-update-test.js
+++ tests/rules/no-object-update-test.js
@@ -8,6 +8,8 @@ const ruleTester = new RuleTester({
   }
 });
 
+const MESSAGE_WITH_OPTIONS = `Use the utility method(s) 'isAlive' OR 'isRemoved' to verify the 'obj' is not being destroyed before calling 'obj.set(...)'`;
+
 ruleTester.run("no-object-update", rule, {
   valid: [
     {
@@ -23,6 +25,63 @@ ruleTester.run("no-object-update", rule, {
             this.set('prop3', false);
           });
         `
+    },
+    {
+      code: `
+        somePromise
+          .then(() => {
+            if(!obj.isDestroying) {
+              obj.set('prop1', true);
+            }
+          })
+          .catch(() => {
+            if(!obj.isDestroying) {
+              if(someOtherCondition()) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          })
+          .finally(() => {
+            if(someOtherCondition()) {
+              if(!obj.isDestroying) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          });
+        `
+    },
+    {
+      code: `
+        somePromise
+          .then(() => {
+            if(!isRemoved(obj)) {
+              obj.set('prop1', true);
+            }
+          })
+          .catch(() => {
+            if(isAlive(obj)) {
+              if(someOtherCondition()) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          })
+          .finally(() => {
+            if(someOtherCondition()) {
+              if(!isMarkedForDelete(obj)) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          });
+        `,
+      options: ["isAlive", "isRemoved", "isMarkedForDelete"]
     }
   ],
   invalid: [
@@ -48,6 +107,79 @@ ruleTester.run("no-object-update", rule, {
         },
         {
           message: MESSAGE
+        }
+      ]
+    },
+    {
+      code: `
+        somePromise
+          .then(() => {
+            if(!isRemoved(obj)) {
+              obj.set('prop1', true);
+            }
+          })
+          .catch(() => {
+            if(isAlive(obj)) {
+              if(someOtherCondition()) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          })
+          .finally(() => {
+            if(someOtherCondition()) {
+              if(!isMarkedForDelete(obj)) {
+                if(someOtherNestedCondition) {
+                  obj.set('prop2', true);
+                }
+              }
+            }
+          });
+        `,
+      errors: [
+        {
+          message: MESSAGE
+        },
+        {
+          message: MESSAGE
+        },
+        {
+          message: MESSAGE
+        }
+      ]
+    },
+    {
+      code: `
+        somePromise
+          .then(() => {
+            obj.set('prop1', true);
+          })
+          .catch(() => {
+            if(someOtherCondition()) {
+              if(someOtherNestedCondition) {
+                obj.set('prop2', true);
+              }
+            }
+          })
+          .finally(() => {
+            if(someOtherCondition()) {
+              if(someOtherNestedCondition) {
+                obj.set('prop2', true);
+              }
+            }
+          });
+        `,
+      options: ["isAlive", "isRemoved"],
+      errors: [
+        {
+          message: MESSAGE_WITH_OPTIONS
+        },
+        {
+          message: MESSAGE_WITH_OPTIONS
+        },
+        {
+          message: MESSAGE_WITH_OPTIONS
         }
       ]
     }
